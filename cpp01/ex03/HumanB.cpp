50c4ad9116defeb3495c24b46b2f9a4bbdf9c01e
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-11-11 05:45:41 +0100

    done

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -3,10 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   HumanB.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zkharbac <zkharbac@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 05:29:53 by zkharbac          #+#    #+#             */
-/*   Updated: 2025/11/09 05:29:54 by zkharbac         ###   ########.fr       */
+/*   Updated: 2025/11/11 04:39:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "HumanB.hpp"
+
+void HumanB::HumanB(std::string newname,Weapon):name(newname),Weapon(Null);
+void HumanB::setWeapon(Weapon& newWeapon)
+{
+    Weapon=&newWeapon;
+}
+void HumanB::attack()
+{
+    if(Weapon)
+        std::cout<<name<<"attack with this type"<<Weapon.settype();
+    else
+        std::cout<<name<<"attack with bare hands\n";
+}
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -3,10 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zkharbac <zkharbac@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 05:24:47 by zkharbac          #+#    #+#             */
-/*   Updated: 2025/11/09 05:24:49 by zkharbac         ###   ########.fr       */
+/*   Updated: 2025/11/11 05:44:31 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <Weapon.hpp>
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main()
+{
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanA bob("Bob", club); 
+        
+        bob.attack(); 
+        
+        club.setType("some other type of club");
+        
+        bob.attack(); 
+    }
+
+    std::cout << "\n----------------------------------------\n" << std::endl;
+
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanB jim("Jim");
+        
+        jim.setWeapon(club); 
+        
+        jim.attack();
+        
+        club.setType("some other type of club");
+        
+        jim.attack();
+    }
+
+    return 0;
+}
